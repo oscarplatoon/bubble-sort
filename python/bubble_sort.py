3c05bf8b8c9c9ea8606dc448b4874a19d1ca235d
JWShuff <16825267+JWShuff@users.noreply.github.com>
2021-06-02 14:19:41 -0400

    enumerate through list printing values and index

--- python/bubble_sort.py
+++ python/bubble_sort.py
@@ -2,8 +2,15 @@ sequence = [4, 3, 5, 0, 1]
 swaps = 0
 
 def bubble_sort(list):
+    current = 0
+    previous = 0
     result_list = []
-    return(result_list)
+    
+    for i, item in enumerate(list):
+        print(list[i])
+        print(i)
 
-# print(f"Final result: {result}")
-# print(f"Swaps: {swaps}")
+    return(result_list)
+result = bubble_sort(sequence)
+print(f"Final result: {result}")
+print(f"Swaps: {swaps}")
