1e88cab95baab60fd75640d6c7ae825daaee2221
vzbrunstad <vzbrunstad@msn.com>
2021-06-02 17:31:28 -0500

    Vince B Bubble Sort

--- python/bubble_sort.py
+++ python/bubble_sort.py
@@ -2,6 +2,21 @@ sequence = [4, 3, 5, 0, 1]
 swaps = 0
 
 # Your Code Here
+x=0
+y=0
+
+while y < (len(sequence)-1):
+# for i, num in enumerate(sequence):
+    if sequence[y]>sequence[y+1]:
+        var1= sequence[y]
+        var2= sequence[y+1]
+        sequence[y] = var2
+        sequence[y+1] = var1
+        y=0
+    else:
+        y=y+1
+result = sequence
+
 
 print(f"Final result: {result}")
-print(f"Swaps: {swaps}")
+# print(f"Swaps: {swaps}")
