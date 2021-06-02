f6d1ab25021da2d46117552e063ac725b447f5c4
Marc Stanley <marcson909@gmail.com>
2021-06-02 18:17:06 -0500

    solved bubble sort with unit tests

--- python/bubble_sort.py
+++ python/bubble_sort.py
@@ -1,7 +1,31 @@
-sequence = [4, 3, 5, 0, 1]
-swaps = 0
+#bubble sort function
+def bubble_sort(array):
+    #flag variable 
+    isSorted = False
+    #swap counter
+    swaps = 0
+    #while array is not sorted execute while loop
+    while not isSorted:
+        #change isSorted to true
+        #if we don't enter the if conditional
+        #this will skip for loop and return array
+        isSorted = True
+        #loop through length of array -1  -swaps
+        #if i greater than i + 1 call swap helper func
+        #comma assignment to swap index positions
+        #flag isSorted back to false
+        for i in range(len(array) - 1 - swaps):
+            if array[i] > array[i + 1]:
+                swap(i, i+1, array)
+                isSorted = False
+        #increment swaps
+        swaps += 1
 
-# Your Code Here
+    return array
 
-print(f"Final result: {result}")
-print(f"Swaps: {swaps}")
+#helper func using comma assignment
+#could have just put it in bubble sort since it's only 1 line
+#but this practices helper functions and I'd do this in JS since it's a little more tedious
+def swap(i, j, array):
+    array[i], array[j] = array[j], array[i]
+
