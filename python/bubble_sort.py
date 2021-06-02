904b9944e620bec3f5c7685a8d8b5d383af4cace
JWShuff <16825267+JWShuff@users.noreply.github.com>
2021-06-02 15:54:57 -0400

    Worst case test implemented and validated.

--- python/bubble_sort.py
+++ python/bubble_sort.py
@@ -1,19 +1,19 @@
-sequence = [4, 3, 5, 0, 1]
-
 def bubble_sort(list):
     swaps = 0
+    input_list = list
     result_list = []
-    length = len(list)
+    length = len(input_list)
     
+    #This i value tracks the "completely ordered to the left" index
     for i in range(length-1):
-        for j in range(0,length-i-1):
-            if list[j] > list[j+1]:
-                list[j], list[j+1] = list[j+1], list[j]
-                swaps += 1
 
-    result_list = [list, swaps]
-    return(result_list, swaps)
-result = bubble_sort(sequence)
-swaps = result[1]
-print(f"Final result: {result}")
-print(f"Swaps: {swaps}")
+        for j in range(0,length-i-1):
+
+            #If term to the right of j is smaller, invert the one to the other
+            if input_list[j] > input_list[j+1]:
+                input_list[j], input_list[j+1] = input_list[j+1], input_list[j]
+
+                swaps += 1
+        
+    result_list = input_list, swaps
+    return(result_list)
--- python/bubble_sort_spec.py
+++ python/bubble_sort_spec.py
@@ -1,22 +1,25 @@
 import unittest
 from bubble_sort import bubble_sort
 
-
-test_list = [4,3,5,0,1]
-
 class TestBubbleSort(unittest.TestCase):
   
     """
-    when you call bubble sort, you get a list back:
+    when you call bubble sort, you get a ([sorted_list], swaps) tuple back:
     """
     def test_returns_a_list(self):
-        self.assertTrue(type(bubble_sort([1])) is list)
+        self.assertTrue(type(bubble_sort([4, 3, 5, 0, 1])) == tuple)
 
     """
     Bubble Sort correctly sorts lists of integers:
     """
     def test_sort_accuracy(self):
-        self.assertTrue(bubble_sort(test_list) == ([0,1,3,4,5]))
+        self.assertTrue(bubble_sort([4, 3, 5, 0, 1])[0] == ([0, 1, 3, 4, 5]))
+
+    """
+    The worst case for bubble sort is when the list is reverse ordered. 15 steps are required.
+    """
+    def test_worst_case(self):
+        self.assertTrue(bubble_sort([5,4,3,2,1, 0]) == ([0,1,2,3,4,5], 15))
 
 if __name__ == '__main__':
     unittest.main()
