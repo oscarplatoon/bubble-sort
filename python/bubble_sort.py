92230a0d54c863fd7d2c4d8a6c244f8cd3123b09
awhitford10 <awhitford@Andrews-Mac-mini.local>
2021-06-02 21:15:55 -0400

    Andrew_bubble sort_ solution

--- python/bubble_sort.py
+++ python/bubble_sort.py
@@ -1,7 +1,22 @@
 sequence = [4, 3, 5, 0, 1]
 swaps = 0
 
-# Your Code Here
 
-print(f"Final result: {result}")
+flag =1
+while flag == 1:
+    prev = 0
+    current = 1
+    flag = 0
+    while current <= (len(sequence)-1):
+        hold = sequence[current]
+        if sequence[prev] > sequence[current]:
+            sequence[current] = sequence[prev]
+            sequence[prev] = hold
+            swaps += 1
+            flag = 1
+        prev += 1
+        current += 1
+
+
+print(f"Final result: {sequence}")
 print(f"Swaps: {swaps}")
