6bda1ab9fb0c6ba2efc0a26cd4498d2a7b1d24ce
AlexanderS68 <gruntmedic68@gmail.com>
2021-06-02 21:20:00 -0400

    JS bubblesort

--- js/bubbleSort.js
+++ js/bubbleSort.js
@@ -1,7 +1,19 @@
-var sequence = [4, 3, 5, 0, 1]
-var swaps = 0
+function bubbleSort(sequence) {
+  let count = 0
+  for (let i = 0; i < sequence.length; i++) {
+    for (let j = 0; j < sequence.length - i - 1; j++) {
+      if (sequence[j] > sequence[j + 1]) {
+        //let currentItem = sequence[j]
+        //let nextItem = sequence[j + 1]
+        let current = sequence[j]
+        sequence[j] = sequence[j + 1]
+        sequence[j + 1] = current
+        count++
+      }
+    }
+  }
+  return sequence, count
+}
 
-// Your Code Here
 
-console.log("Final result: " + result)
-console.log("Swaps: " + swaps)
+console.log((bubbleSort([1, 7, 8, 6, 3, 2, 4])))
